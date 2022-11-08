ca5c8304034699875d802f2e62a5cec6e64916f0
ANURAG PANDEY <59971421+JOSBEAK@users.noreply.github.com>
2022-11-08 13:21:50 +0530

    Update func1.rs

--- module1/src/bin/func1.rs
+++ module1/src/bin/func1.rs
@@ -9,7 +9,15 @@
 // * Use the println macro to display messages to the terminal
 
 fn main() {
-
+   first_name();
+   second_name();
 }
-
+fn first_name()  {
+    let s = String::from("Anurag");
+    println!("{}", s);
+}
+fn second_name() {
+    let s = String::from("Pandey");
+    println!("{}", s);
+}
 
